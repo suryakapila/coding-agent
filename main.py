1dceed30438801bceb9a9be96ac93d4fb0a61aca
suryakapila <surya.kapila@gmail.com>
2026-02-07 13:00:11 +0000

    Added logic to take user inputs and include verbose flag to see the nerd stats

--- main.py
+++ main.py
@@ -1,23 +1,41 @@
 import os
 from dotenv import load_dotenv
-
+import argparse
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.getenv("GEMINI_API_KEY")
 
-
-
 def main():
     print("Hello from coding-agent!")
     if api_key is None:
-        raise RuntimeError("No API key found. Please set GEMINI_API_KEY in your .env file.") 
-    client = genai.Client(api_key = api_key) 
+        raise RuntimeError("No API key found. Please set GEMINI_API_KEY in your .env file.")
+
+    # input from user
+    parser = argparse.ArgumentParser(description='Chatbot')
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument('--verbose',action='store_true', help='Enable verbose output')
+    args = parser.parse_args()
+
+    # make API call to Gemini 2.5 flash
+    client = genai.Client(api_key=api_key)
+
+    # roles are user
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
     response = client.models.generate_content(
     model='gemini-2.5-flash', 
-    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    contents=messages
     )
-    print(response.text)  
+    x = response.usage_metadata
+    if x is None:
+        raise RuntimeError("No usage metadata found in response. Like failed API request?")
+    if args.verbose:
+        print("User prompt: ", messages[0].parts[0].text)
+        print("Prompt tokens: ", x.prompt_token_count)
+        print("Response tokens: ", x.candidates_token_count)
+        
+    print("Response: ", response.text)
 
 
 if __name__ == "__main__":
