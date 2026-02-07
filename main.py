ccafadb190cc9b647f7ab9621435102aa78d8119
suryakapila <surya.kapila@gmail.com>
2026-02-07 11:40:44 +0000

    Added gemini 2.5 flash llm  and generated a response using client.models.generate_content()

--- main.py
+++ main.py
@@ -1,5 +1,23 @@
+import os
+from dotenv import load_dotenv
+
+from google import genai
+
+load_dotenv()
+api_key = os.getenv("GEMINI_API_KEY")
+
+
+
 def main():
     print("Hello from coding-agent!")
+    if api_key is None:
+        raise RuntimeError("No API key found. Please set GEMINI_API_KEY in your .env file.") 
+    client = genai.Client(api_key = api_key) 
+    response = client.models.generate_content(
+    model='gemini-2.5-flash', 
+    contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    )
+    print(response.text)  
 
 
 if __name__ == "__main__":
